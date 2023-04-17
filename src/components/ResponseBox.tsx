9b56da9f4359fff4372bd323b4add03f29bd5965
emilyglue <emily.gu@nyu.edu>
2023-04-17 15:41:32 -0500

    more cleanup

--- src/components/ResponseBox.tsx
+++ src/components/ResponseBox.tsx
@@ -21,15 +21,15 @@ const ResponseBox = ({
   const response = responseList[responseKey]
 
   return (
-    <ColumnBox style={{ marginTop: 'calc(100vh / 4)' }}>
-      <div className='hideOverflow' key={bobaKey}>
+    <ColumnBox maxHeight='600px'>
+      <ColumnBox className='hideOverflow' key={bobaKey}>
         <Response subtitleText={response.subtitle} titleText={response.title} />
         <ColumnBox>
           <Button onClick={onClick} style={{ margin: '2rem' }}>
             {shouldGetBoba ? 'r u sure' : 'I need a second opinion'}
           </Button>
         </ColumnBox>
-      </div>
+      </ColumnBox>
     </ColumnBox>
   )
 }
